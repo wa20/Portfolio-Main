940980c9492c8e9071a55c64723feb2f9d83eee9
wael <78425538+wa20@users.noreply.github.com>
2021-08-23 19:51:39 +0100

    CLIENT: project schema created and populated, project card created

---
+++ client/src/components/tempPortfolio/projectCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+// import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Button, Icon } from "semantic-ui-react";
+import ProjectSchema from "./projectSchema";
+import "./projectCard.css";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    minWidth: 345,
+  },
+  media: {
+    height: 200,
+  },
+});
+
+export default function ProjectCard() {
+  const classes = useStyles();
+
+  return (
+    <div className="grid">
+      {ProjectSchema.map((project) => (
+        <div>
+          <Card
+            className={classes.root}
+            style={{ margin: "20px", marginBottom: "10px" }}
+          >
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={project.image}
+                title={project.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {project.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {project.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button href={`${project.repo}`} variant="outlined"><Icon disabled name="github"/> View Repo</Button>
+     <Button href={`${project.site}`} variant="outlined"><Icon disabled name="tv"/> View Site</Button>
+            </CardActions>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ client/src/components/tempPortfolio/projectSchema.js
@@ -0,0 +1,132 @@
+import React from "react"
+// import {codeWallpaper} from "./assets/codeWallpaper.png"
+
+const codeWallpaper = require("../../assets/projectImages/codeWallpaper.png").default;
+const dailyPlanner = require("../../assets/projectImages/dailyPlanner.png").default;
+const FitnessApp = require("../../assets/projectImages/fitnessTracker.png").default;
+const PWGen = require("../../assets/projectImages/PWGenerator.png").default;
+const TimedQuiz = require("../../assets/projectImages/timedQuiz.png").default;
+const wrkWith = require("../../assets/projectImages/wrkwith.png").default;
+const BookApp = require("../../assets/projectImages/bookapp.png").default;
+const bdgTracker = require("../../assets/projectImages/budgetTracker.png").default;
+
+
+//temp solution to query projects
+const ProjectSchema =  [
+    {
+        id: 1,
+        name: "wrkwith-app",
+        image: wrkWith,
+        description:"A fully functioning website utilisng MVC model",
+        site: "https://wrkwith-app.herokuapp.com",
+        repo: "https://github.com/wa20/wrkwith-app",
+        detail: "A fully functioning website utilisng MVC model",
+
+    },
+    {
+        id: 2,
+        name: "BookApp",
+        image: BookApp,
+        description:"",
+        site: "https://wa20.github.io/BookApp/",
+        repo: "https://github.com/wa20/BookApp",
+        detail: "",
+
+    },
+    {
+        id: 3,
+        name: "Node README Generator",
+        image: codeWallpaper,
+        site: "",
+        description:"",
+        repo: "https://github.com/wa20/7-README-Generator",
+        detail: "",
+
+    },
+    {
+        id: 4,
+        name: "The Budget Tracker",
+        image: bdgTracker,
+        description:"",
+        site: "https://thebudget-tracker.herokuapp.com",
+        repo: "https://github.com/wa20/budget-tracker",
+        detail: "",
+
+    },
+    {
+        id: 5,
+        name: "The Fitness Tracker",
+        image: FitnessApp,
+        description:"",
+        site: "https://the-fitnesstracker.herokuapp.com/?id=60e43ebb51bc3a00155d11ac",
+        repo: "https://github.com/wa20/the-fitness-tracker",
+        detail: "",
+
+    },
+
+    {
+        id: 6,
+        name: "What is 'Regex'?",
+        image: codeWallpaper,
+        site: "",
+        description:"",
+        repo: "https://github.com/wa20/regex-tutorial",
+        detail: "",
+
+    },
+
+    {
+        id: 7,
+        name: "Node Profile Generator",
+        image: codeWallpaper,
+        site: "",
+        description:"",
+        repo: "https://github.com/wa20/8-Team-Profile-Generator",
+        detail: "",
+
+    },
+    {
+        id: 8,
+        name: "Ecom-site Backend",
+        image: codeWallpaper,
+        site: "",
+        description:"",
+        repo: "https://github.com/wa20/11-E-Commerce-Site",
+        detail: "",
+
+    },
+    {
+        id: 9,
+        name: "Password Generator",
+        image: PWGen,
+        description:"",
+        site: "https://wa20.github.io/3-Password-Generator/",
+        repo: "https://github.com/wa20/3-Password-Generator",
+        detail: "",
+
+    },
+    {
+        id: 10,
+        name: "Timed Quiz",
+        image: TimedQuiz,
+        description:"",
+        site: "https://wa20.github.io/4-Timed-Quiz/",
+        repo: "https://github.com/wa20/4-Timed-Quiz",
+        detail: "",
+
+    },
+    {
+        id: 11,
+        name: "Daily Planner",
+        image: dailyPlanner,
+        description:"",
+        site: "https://github.com/wa20/5-Daily-Planner/settings/pages",
+        repo: "https://github.com/wa20/5-Daily-Planner",
+        detail: "",
+
+    },
+
+
+]
+
+export default ProjectSchema
