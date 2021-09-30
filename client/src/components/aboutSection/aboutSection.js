618fe56bd3061d7705714a3250021ec15d9baffb
wael <78425538+wa20@users.noreply.github.com>
2021-09-30 17:18:47 +0100

    STYLING: updated about section

--- client/src/components/aboutSection/aboutSection.js
+++ client/src/components/aboutSection/aboutSection.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import DevIcon from "devicon-react-svg";
-import { Grid, Header, Image, Segment } from "semantic-ui-react";
-
+import { Grid, Header, Image, Segment, Popup } from "semantic-ui-react";
 import './aboutSection.css'
 
 const bootstrap = require("../../assets/techSVGLogo/bootstrap-plain-wordmark.svg").default;
@@ -13,7 +11,7 @@ const github = require("../../assets/techSVGLogo/github-original-wordmark.svg").
 const graphQL = require("../../assets/techSVGLogo/graphql-plain-wordmark.svg").default;
 const handlebars = require("../../assets/techSVGLogo/handlebars-original-wordmark.svg").default;
 const html5 = require("../../assets/techSVGLogo/html5-plain-wordmark.svg").default;
-const illustraitor = require("../../assets/techSVGLogo/illustrator-plain.svg").default;
+const illustrator = require("../../assets/techSVGLogo/illustrator-plain.svg").default;
 const javascript = require("../../assets/techSVGLogo/javascript-original.svg").default;
 const jquery = require("../../assets/techSVGLogo/jquery-original-wordmark.svg").default;
 const materialui = require("../../assets/techSVGLogo/materialui-original.svg").default;
@@ -53,67 +51,170 @@ const AboutSection = () => {
             <Grid style={{ fontSize: "1.33em" }}>
               <Grid.Row columns={4}>
                 <Grid.Column>
-                  <Image size="tiny" src={react} alt={react} className="scale"/>
+                <Popup
+                  trigger={<Image size="tiny" src={react} alt={react} className="scale"/>}
+                  content='React.js'
+                  position='top center'
+                  inverted
+                />
+
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={graphQL} alt={graphQL} className="scale" />
+                  <Popup
+                  trigger={<Image size="tiny" src={graphQL} alt={graphQL} className="scale" />}
+                  content='GraphQL'
+                  position='top center'
+                  inverted
+                />
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={nodejs} alt={nodejs} className="scale"/>
+                <Popup
+                  trigger={ <Image size="tiny" src={nodejs} alt={nodejs} className="scale"/>}
+                  content='Node.js'
+                  position='top center'
+                  inverted
+                />                 
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={javascript} alt={javascript} className="scale"/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={5}>
-                <Grid.Column>
-                  <Image size="tiny" src={express} alt={express} className="scale" />
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="small" src={sequelize} alt={sequelize} className="scale" />
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={mongodb} alt={mongodb} className="scale" />
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={mysql} alt={mysql} className="scale"/>
-                </Grid.Column>
-                <Grid.Column>
-                <Image size="tiny" src={handlebars} alt={handlebars} className="scale" />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={5}>
-                <Grid.Column>
-                  <Image size="tiny" src={jquery} alt={jquery} className="scale"/>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={css3} alt={css3} className="scale"/>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={html5} alt={html5} className="scale"/>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={git} alt={git} className="scale" />
-                </Grid.Column>
-                <Grid.Column>
-                  <Image size="tiny" src={github} alt={github} className="scale"/>
+                <Popup
+                  trigger={<Image size="tiny" src={javascript} alt={javascript} className="scale"/>}
+                  content='Javascript ES6+'
+                  position='top center'
+                  inverted
+                />                 
                 </Grid.Column>
               </Grid.Row>
               <Grid.Row columns={5}>
                 <Grid.Column>
-                  <Image size="tiny" src={bootstrap} alt={bootstrap} className="scale"/>
+                <Popup
+                  trigger={<Image size="tiny" src={express} alt={express} className="scale" />}
+                  content='Express'
+                  position='top center'
+                  inverted
+                />                
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={materialui} alt={materialui} className="scale"/>
+                <Popup
+                  trigger={<Image size="small" src={sequelize} alt={sequelize} className="scale" />}
+                  content='Sequelize'
+                  position='top center'
+                  inverted
+                />                 
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={bulma} alt={bulma} className="scale"/>
+                <Popup
+                  trigger={<Image size="tiny" src={mongodb} alt={mongodb} className="scale" />}
+                  content='Sequelize'
+                  position='top center'
+                  inverted
+                /> 
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={illustraitor} alt={illustraitor} className="scale"/>
+                <Popup
+                  trigger={ <Image size="tiny" src={mysql} alt={mysql} className="scale"/>}
+                  content='mySQL'
+                  position='top center'
+                  inverted
+                />               
                 </Grid.Column>
                 <Grid.Column>
-                  <Image size="tiny" src={photoshop} alt={photoshop} className="scale"/>
+                <Popup
+                  trigger={  <Image size="tiny" src={handlebars} alt={handlebars} className="scale" />}
+                  content='Handlebars'
+                  position='top center'
+                  inverted
+                />
+              
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={5}>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={jquery} alt={jquery} className="scale"/>}
+                  content='jQuery'
+                  position='top center'
+                  inverted
+                />              
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={css3} alt={css3} className="scale"/>}
+                  content='CSS3'
+                  position='top center'
+                  inverted
+                />             
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={html5} alt={html5} className="scale"/>}
+                  content='HTML5'
+                  position='top center'
+                  inverted
+                />
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={git} alt={git} className="scale" />}
+                  content='Git'
+                  position='top center'
+                  inverted
+                />             
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={github} alt={github} className="scale"/>}
+                  content='GitHub'
+                  position='top center'
+                  inverted
+                />
+                  
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={5}>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={bootstrap} alt={bootstrap} className="scale"/>}
+                  content='Bootstrap'
+                  position='top center'
+                  inverted
+                />
+                  
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={materialui} alt={materialui} className="scale"/>}
+                  content='Material UI'
+                  position='top center'
+                  inverted
+                />
+                  
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={bulma} alt={bulma} className="scale"/>}
+                  content='bulma'
+                  position='top center'
+                  inverted
+                />
+                  
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={illustrator} alt={illustrator} className="scale"/>}
+                  content='Adobe Illustrator'
+                  position='top center'
+                  inverted
+                />
+                  
+                </Grid.Column>
+                <Grid.Column>
+                <Popup
+                  trigger={<Image size="tiny" src={photoshop} alt={photoshop} className="scale"/>}
+                  content='Adobe Photoshop'
+                  position='top center'
+                  inverted
+                />
+                  
                 </Grid.Column>
               </Grid.Row>
             </Grid>
